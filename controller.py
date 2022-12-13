04661a85574ca0be647f6b298e65682d749ff588
Oleg_bul <kokseg63@gmail.com>
2022-12-14 01:42:31 +0400

    init commit

---
+++ controller.py
@@ -0,0 +1,15 @@
+import model
+import view
+import logger
+
+
+# def run():
+#     mode = view.choose_mode()
+#     if mode == 'Крестики нолики':
+#         result = model.run_game()
+#         view.show_results(result)
+#         logger.log_result(result)
+#     if mode == 'Конфеты':
+#         result = game2.run_game()
+#         view.show_results(result)
+#         logger.log_result(result)
